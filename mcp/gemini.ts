c138b1b2be62f8f4b4966ea52f290207e6035742
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-09-27 21:17:58 -0400

    reinit gemini

--- mcp/gemini.ts
+++ mcp/gemini.ts
@@ -1,3 +1,30 @@
 import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
+import readline from "readline";
+import { Readable, Writable } from "stream";
 
-const ai = new GoogleGenAI({});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+async function main() {
+  const chat = ai.chats.create({
+    model: "gemini-2.5-flash",
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+  });
+
+  const response1 = await chat.sendMessage({
+    message: "I have 2 dogs in my house.",
+  });
+  console.log("Chat response 1:", response1.text);
+}
+
+await main();
